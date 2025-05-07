ce2e4d9be3f16d1e0e992644f7444d108abc9f82
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2025-05-07 19:33:41 +0300

    Make HTML and JS works

--- soloProject-PasswordGenerator/index.js
+++ soloProject-PasswordGenerator/index.js
@@ -1,4 +1,32 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", 
+    "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", 
+    "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", 
+    "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", 
+    "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", 
+    ",", "|", ":", ";", "<", ">", ".", "?", "/"];
 
-console.log("JS work")
+const generateBtn = document.getElementById("generate-btn");
+const p1El = document.getElementById("p1");
+const p2El = document.getElementById("p2");
+
+let passwordLenght = 15;
+
+
+generateBtn.addEventListener("click", function(){
+    p1El.textContent = generateRandomPassword()
+
+    p2El.textContent = generateRandomPassword()
+})
+
+function generateRandomPassword() {
+    let password = ""
+    for (let i = 0; i < passwordLenght; i++) {
+        password += characters[getRandomChar()]
+    }
+
+    return password;
+}
+
+function getRandomChar() {
+    return Math.floor( Math.random() * characters.length ) + 1;
+}
