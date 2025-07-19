5ed17a8a049705e67f4cf9a4df46a9dcbd9245ce
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2025-07-19 04:55:25 +0300

    Display camera make and location in author section

--- capstone/index.js
+++ capstone/index.js
@@ -3,6 +3,10 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
 		document.getElementById("author").textContent = `By: ${data.user.name}`
+        document.getElementById("author").innerHTML += `
+        <p>${data.exif.make}<p>
+        <p>${data.location.name}<p>
+        `
     })
     .catch(err => {
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
