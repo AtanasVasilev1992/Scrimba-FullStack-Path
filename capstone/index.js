090a30d244138d86c338ef0f636f6b3c9f94ccec
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2025-07-20 00:03:22 +0300

    Add live time display and refactor DOM access
    Introduced a live time display by updating the time every second and refactored DOM element access to use variables for improved readability and performance. Also added missing semicolons and improved error handling consistency.

--- capstone/index.js
+++ capstone/index.js
@@ -1,32 +1,48 @@
+const body = document.body;
+const author = document.getElementById("author");
+const time = document.getElementById("time");
+const crypto = document.getElementById("crypto");
+const cryptoTop = document.getElementById("crypto-top");
+
+
+function updateTime() {
+    const date = new Date()
+    let currentTime = date.toLocaleTimeString("bg-BG", {timeStyle: "medium"})
+    time.textContent = currentTime
+};
+
+setInterval(updateTime, 1000);
+
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
-        document.getElementById("author").innerHTML += `
+        body.style.backgroundImage = `url(${data.urls.regular})`
+		author.textContent = `By: ${data.user.name}`
+        author.innerHTML += `
         <p>${data.exif.make}<p>
         <p>${data.location.name}<p>
         `
     })
     .catch(err => {
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+        body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
-    })
+		author.textContent = `By: Dodi Achmad`
+    });
 
 
 fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     .then(res => res.json())
     .then(data => {
-         document.getElementById("crypto-top").innerHTML = `
+         cryptoTop.innerHTML = `
             <img src=${data.image.small} />
             <span>${data.name}</span>
         `
 
-        document.getElementById("crypto").innerHTML += `
+        crypto.innerHTML += `
             <p>🎯: € ${data.market_data.current_price.eur}</p>
             <p>👆: € ${data.market_data.high_24h.eur}</p>
             <p>👇: € ${data.market_data.low_24h.eur}</p>
         `
     })
-    .catch(err => console.error(err))
+    .catch(err => console.error(err));
+
