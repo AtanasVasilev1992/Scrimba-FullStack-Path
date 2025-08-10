dfcbf82bee2753f42b910e28c4de392ae33f2387
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2025-08-10 21:14:23 +0300

    Convert to ES modules and add basic HTTP server

--- wildHorizonsApi/server.js
+++ wildHorizonsApi/server.js
@@ -1,1 +1,11 @@
-console.log('Server works!')
+import http from 'node:http';
+
+const PORT = 8000;
+
+const server = http.createServer((req, res)=> {
+    if (req.url === '/api' && req.method === 'GET') {
+        res.end('This is from server!')
+    }
+})
+
+server.listen(PORT, ()=> console.log(`Server is listennig on port: ${PORT}`))
