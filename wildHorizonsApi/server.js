27035529c1472da85f9e229fbadef6f9792092b5
Atanas Vasilev <148366163+AtanasVasilev1992@users.noreply.github.com>
2025-08-12 22:55:22 +0300

    Get data from DB

--- wildHorizonsApi/server.js
+++ wildHorizonsApi/server.js
@@ -1,11 +1,16 @@
 import http from 'node:http';
+import { getDataFromDB } from './database/db';
 
 const PORT = 8000;
 
-const server = http.createServer((req, res)=> {
+const server = http.createServer(async (req, res)=> {
+    const destinations = await getDataFromDB();
+
     if (req.url === '/api' && req.method === 'GET') {
-        res.end('This is from server!')
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 200;
+        res.end(JSON.stringify(destinations));
     }
-})
+});
 
 server.listen(PORT, ()=> console.log(`Server is listennig on port: ${PORT}`))
