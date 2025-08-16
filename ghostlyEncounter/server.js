5923b212a931b515284250e0130bb27e97d3ce90
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2025-08-16 22:38:45 +0300

    Implement static file serving with content type handling
    Refactored the server to serve static files from the public directory using a new serveStatic utility. Added getContentType and sendResponse utilities to handle content type resolution and response sending. The server now returns appropriate content types, serves 404.html for missing files, and handles server errors gracefully.

--- ghostlyEncounter/server.js
+++ ghostlyEncounter/server.js
@@ -1,15 +1,13 @@
-import http from 'node:http';
+import http from 'node:http'
+import { serveStatic } from './utils/serveStatic.js'
 
-import { serveStatic } from './utils/serveStatic.js';
+const PORT = 8000
 
-const PORT = 8000;
-const __dirname = import.meta.dirname;
+const __dirname = import.meta.dirname
 
-const server = http.createServer((req, res)=> {
-    serveStatic(__dirname);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><h1>The server is working</h1></html>');
-})
+const server = http.createServer(async (req, res) => {
+    await serveStatic(req, res, __dirname)
+}) 
+
 
 server.listen(PORT, ()=> console.log(`Server is listen on port: ${PORT}`));
