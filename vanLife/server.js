fde08b880669bbd9b0f2f3fff6b0e6855cb1baa0
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2026-03-02 19:24:49 +0200

    Pass request to requireAuth and preserve redirect

--- vanLife/server.js
+++ vanLife/server.js
@@ -45,9 +45,7 @@ createServer({
 
         this.post("/login", (schema, request) => {
             const { email, password } = JSON.parse(request.requestBody)
-            // This is an extremely naive version of authentication. Please don't
-            // do this in the real world, and never save raw text passwords
-            // in your database 😇
+            // This is an extremely bad version of authentication. 
             const foundUser = schema.users.findBy({ email, password })
             if (!foundUser) {
                 return new Response(401, {}, { message: "No user with those credentials found!" })
